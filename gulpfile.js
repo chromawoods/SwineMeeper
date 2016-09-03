e8655af5ff4d8bff5eb51d11a3f0704e00b4f890
Andreas Weber <andreas@chromawoods.com>
2016-09-03 23:47:53 +0200

    Added Gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('scripts', function() {
+
+  gulp.src('./scripts/src/*.js')
+    .pipe(concat('SwineMeeper.min.js'))
+    .pipe(uglify({
+      preserveComments: 'license'
+    }))
+    .pipe(gulp.dest('./scripts/'));
+
+});
+
+gulp.task('default', function() {
+  gulp.watch('./scripts/src/*.js', ['scripts']);
+});
