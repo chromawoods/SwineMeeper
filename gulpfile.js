4b5ce8ec37efb2720c3f6b7330a55d883bcf9050
Andreas Weber <andreas@chromawoods.com>
2016-10-07 22:28:40 +0200

    Added gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+
 
 gulp.task('scripts', function() {
 
-  gulp.src(['./scripts/src/license.js', './scripts/src/*.js'])
+  gulp.src(['./scripts/src/license.js', './scripts/src/**/*.js'])
     .pipe(concat('SwineMeeper.min.js'))
     .pipe(uglify({
       preserveComments: 'license'
@@ -13,6 +15,21 @@ gulp.task('scripts', function() {
 
 });
 
+
+gulp.task('styles', function() {
+
+  gulp.src('./scss/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest('./stylesheets'))
+
+});
+
+
 gulp.task('default', function() {
-  gulp.watch('./scripts/src/*.js', ['scripts']);
+
+  gulp.watch('./scripts/src/**/*.js', ['scripts']);
+  gulp.watch('./scss/**/*.scss', ['styles']);
+
 });
